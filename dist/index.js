cad1f5d71227d950c1ea629127bd2665c4288714
0-Chan <kyc4421194@gmail.com>
2022-10-31 19:40:58 +0900

    Fix search bar text holder

--- dist/index.js
+++ dist/index.js
@@ -238,7 +238,7 @@ var Search = ({ directories = [] }) => {
     className: "hidden sm:flex absolute inset-y-0 right-0 py-1.5 pr-1.5 select-none pointer-events-none"
   }, /* @__PURE__ */ React4.createElement("kbd", {
     className: "inline-flex items-center px-1.5 font-mono text-sm font-medium bg-white text-gray-400 dark:text-gray-800 dark:border-gray-400 border rounded"
-  }, "/"))), renderList && /* @__PURE__ */ React4.createElement("ul", {
+  }, "\u{1F50E}"))), renderList && /* @__PURE__ */ React4.createElement("ul", {
     className: "absolute left-0 z-20 w-full p-0 py-2.5 m-0 mt-1 list-none border divide-y rounded shadow-md md:right-0 top-100 md:w-auto"
   }, results.map((res, i) => {
     return /* @__PURE__ */ React4.createElement(Item, {
@@ -1891,7 +1891,7 @@ var defaultTheme = {
     if (locale === "zh-CN")
       return "\u641C\u7D22\u6587\u6863...";
     if (locale === "ko")
-      return "\uAC80\uC0C9\uCC3D\uC73C\uB85C \uC774\uB3D9\uD558\uB824\uBA74 / \uD0A4\uB97C \uB204\uB974\uC138\uC694.";
+      return "\uAC80\uC0C9\uCC3D\uC73C\uB85C \uC774\uB3D9\uC740 / \uD0A4";
     return "Search documentation...";
   },
   unstable_searchResultEmpty: () => /* @__PURE__ */ React26.createElement("span", {
